632a3f3aeecfca9ee8fd7c44ac04674c1ebfa5fb
20190511 <baeghmath@soonsil.ac.kr>
2021-02-06 21:00:21 +0900

    연결리스트를 활용한 클래스(노드) 큐 구현

---
+++ 2. 트리/parameterExample.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+
+//오류 발생. 
+void example (int inte=1, int int2)
+{
+	cout<<"int2 ="<<int2<<", inte = "<<inte<<endl;
+}
+
+
+int main (void)
+{
+	example(3);
+	return 1;
+}
---
+++ 2. 트리/tree.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <string>
+using namespace std;
+/*
+	트리 기본구조:
+
+*/
+
+
+
+class Node {
+public:
+	int data;
+	Node *left;
+	Node *right;
+	
+	Node(int value = 0)
+	{
+		this->data = value;
+		this->left = NULL;
+		this->right = NULL;
+	}
+};
+
+
+class Tree {
+public:
+	Node *root;
+	Tree (int value = 0)
+	{
+		this->root->data = value; //data 지정. 
+		this->root->left = NULL;
+		this->root->right = NULL;
+	}
+	
+	void preOrder (Node *node = root)
+	{
+		if(root != NULL)
+		{
+			cout<<root->data <<" ";
+			preOrder(root->leftChild);
+			preOrder(root->rightChild);
+		}
+	}
+	
+	
+	void inOrder (Node *node = root)
+	{
+		if(root != NULL)
+		{
+			inOrder(root->leftChild);
+			cout<<root->data<<" ";
+			inOrder(root->rightChild);
+		}
+	}
+	
+	
+	void postOrder (Node *node = root)
+	{
+		if(root != NULL)
+		{
+			postOrder(root->leftChild);
+			postOrder(root->rightChild);
+			cout<<root->data<<" ");
+		}
+	}
+
+	
+	void justInsert (int value)
+	{
+		Node *rememberRoot = root;
+		if(root == NULL)
+		{
+			root->data = value;
+			root->left = NULL;
+			root->right = NULL;
+		}
+		else
+		{
+			while(1)
+			{
+				if(root->left == NULL)
+				{
+					
+				}
+				else if(root->right == NULL)
+				{
+					
+				}
+				else
+				{
+					
+				}
+			}
+		}
+	}	
+};
+
+
+
+
+
+
+
+int main(void)
+{
+	
+	
+	return  1;
+}
